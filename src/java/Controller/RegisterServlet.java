1ccc73292a50b612c06959a87e4de456dfa9e0a6
Nguyen Trung Nghia <nghianthe150495@fpt.edu.vn>
2025-04-17 16:41:44 +0700

    Update Tomcat 11

--- src/java/Controller/RegisterServlet.java
+++ src/java/Controller/RegisterServlet.java
@@ -1,8 +1,10 @@
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+package Controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet("/register")
